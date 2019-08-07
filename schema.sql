53f5e4d209e7406a814d4d3b2822f16d9fb21e40
unknown <serruhb@gmail.com>
2019-08-07 18:32:44 -0500

    Updated code with connection

---
+++ schema.sql
@@ -0,0 +1,17 @@
+--Create database austin_animal_center
+
+CREATE DATABASE austin_animal_center;
+CREATE DATABASE
+
+--Create table intakes
+
+CREATE TABLE intakes(
+Animal_ID VARCHAR PRIMARY KEY,
+Name VARCHAR,
+DateTime VARCHAR,
+Intake_Type VARCHAR,
+Intake_Condition VARCHAR,
+Age VARCHAR,
+Breed VARCHAR,
+Color VARCHAR);
+CREATE TABLE
